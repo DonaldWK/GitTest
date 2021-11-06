0d03949f96247a737d4cd9f7a090d2e23602c8f2
littleme <1603236707@qq.com>
2021-11-06 22:23:47 +0800

    第一次提交

---
+++ src/main/java/com/little/MyGit.java
@@ -0,0 +1,13 @@
+package com.little;
+
+/**
+ * @Author: Donald
+ * @Date: 2021/11/6 22:20
+ */
+
+public class MyGit {
+  public static void main(String[] args) {
+    //
+    System.out.println("我的第一个程序");
+  }
+}
